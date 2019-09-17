87c9c4f403391379feace60cb3744c53c56ee4f6
SunQi0415 <sunqi9@100tal.com>
2019-09-17 10:56:13 +0800

    暴露login registerPage方法

--- lib/index.ts
+++ lib/index.ts
@@ -9,9 +9,9 @@ interface autoTrackConfig {
   userId?: string | number; // 用户id
   [propName: string]: any;
 }
-function SaInit(options: autoTrackConfig):void {
+function saAutoTrack(params: autoTrackConfig):void {
   sa.init({
-    server_url: options.serverUrl,
+    server_url: params.serverUrl,
     heatmap: {
       clickmap: 'default', // 是否开启点击图，默认 default 表示开启，自动采集 $WebClick 事件，可以设置 'not_collect' 表示关闭
       scroll_notice_map: 'default' // 是否开启触达注意力图，默认 default 表示开启，自动采集 $WebStay 事件，可以设置 'not_collect' 表示关闭
@@ -23,42 +23,62 @@ function SaInit(options: autoTrackConfig):void {
     is_track_single_page: true // 单页面中自动采集web浏览事件$pageview
   })
   // 标识用户
-  if (options.userId) { sa.login(options.userId) }
+  if (params.userId) { sa.login(params.userId) }
   // 全埋点
   sa.quick('autoTrack')
 
-  /* 预置采集属性 */
   ;(async () => {
-    // 传入的参数
-    let optionsCopyData = options;
-    delete optionsCopyData.serverUrl
-    // performance相关数据采集
+    // performance数据上报
     let pdc: PDC = await new PDC()
     let {whiteScreenTime, domReadyTime, requestTime, onLoadTime} = pdc
-    // navigator相关数据采集
-    let ndc: NDC = await new NDC()
+    saTrack('performance', {
+      whiteScreenTime: whiteScreenTime,
+      domReadyTime: domReadyTime,
+      requestTime: requestTime,
+      onLoadTime: onLoadTime
+    })
+
+    /* 预置采集事件公共属性 */
+    let paramsCopyData = params;
+    delete paramsCopyData.serverUrl // 删除传入参数的serverUrl
+    let ndc: NDC = await new NDC() // navigator相关数据采集
+
     sa.registerPage(
-      Object.assign(optionsCopyData, {
+      Object.assign(paramsCopyData, {
         gps_lon: ndc.gpsLon, // 经度
         gps_lat: ndc.gpsLat, // 纬度
         current_url: location.href, // 当前url
         referrer: document.referrer, // 前一个url
         network_type: ndc.networkType, // 网络类型
-        domain_name: document.domain, // 域名
-        whiteScreenTime: whiteScreenTime,
-        domReadyTime: domReadyTime,
-        requestTime: requestTime,
-        onLoadTime: onLoadTime
+        domain_name: document.domain // 域名
       })
     )
   })()
 }
+
 // 自定义事件追踪
 interface trackInterface {
   (eventName: string, properties?: object, callback?: any): void
 }
-let SaTrack: trackInterface = function(eventName: string, properties: object, callback: Function): void {
+
+let saTrack: trackInterface = function(eventName: string, properties: object, callback: Function): void {
   sa.track(eventName, properties, callback)
+}
+
+// 设置用户属性
+function saSetProfile(options: any) {
+  sa.setProfile(options)
+}
+
+// 匿名ID和登录ID关联
+function saLogin(userId: string | number) {
+  sa.login(userId)
+  sa.quick('autoTrack')
+}
+
+// 设置事件公共属性
+function saRegisterPage(params: any) {
+  sa.registerPage(params)
 }
 
 // 可视区域停留时长
@@ -92,7 +112,13 @@ class StayDuration implements stayDurationInterface {
       let scrollTop = document.documentElement.scrollTop || document.body.scrollTop // 页面卷起高度
       this.calcDuration(scrollTop)
     }, 1000), false)
+    // 监听页面url跳转和页面关闭
+    window.addEventListener('popstate', this.endReport, false)
+    window.addEventListener('hashchange,', this.endReport, false)
+    window.addEventListener('beforeunload', this.endReport, false)
+    window.addEventListener('unload', this.endReport, false)
   }
+  // 计算区域停留时长
   calcDuration(scrolltop: number) {
     let {contentHeight, scrollTop, contentOffsetTop, contentViewTop} = this.domAttribute(scrolltop)
     let distance = StayDuration.clientHeight - contentViewTop
@@ -101,46 +127,31 @@ class StayDuration implements stayDurationInterface {
     if (distance >= contentHeight * this.percent / 100 && distance <= StayDuration.clientHeight) {
       this.flag = true
       this.timer = setInterval(()=> {
-        // 结束计时
-        window.addEventListener('popstate', () => {
-          this.flag = false
-          clearInterval(this.timer)
-          SaTrack(this.eventName, Object.assign({
-            stay_duration: this.duration
-          }, this.props))
-          return
-        }, false)
-        // 结束计时
-        window.addEventListener('hashchange', () => {
-          this.flag = false
-          clearInterval(this.timer)
-          SaTrack(this.eventName, Object.assign({
-            stay_duration: this.duration
-          }, this.props))
-          return
-        }, false)
-        // 结束计时
-        window.addEventListener('unload', () => {
-          this.flag = false
-          clearInterval(this.timer)
-          SaTrack(this.eventName, Object.assign({
-            stay_duration: this.duration
-          }, this.props))
-          return
-        }, false)
+        // console.log('开始计时')
         this.duration++
       }, 1000)
     }
     // 结束计时
     if (scrollTop - contentOffsetTop > contentHeight * (1 - this.percent / 100)) {
       if(this.flag) {
+        // console.log('结束计时')
         this.flag = false
-        SaTrack(this.eventName, Object.assign({
+        saTrack(this.eventName, Object.assign({
           stay_duration: this.duration
         }, this.props))
       }
       return
     }
+  }
+  // 结束数据上报
+  endReport() {
+    this.flag = false
+    clearInterval(this.timer)
+    saTrack(this.eventName, Object.assign({
+      stay_duration: this.duration
+    }, this.props))
+    this.duration = 0
+    return
   }
   domAttribute(scrollTop: number) {
     let content = document.getElementById(this.elementId) // 内容元素id
@@ -163,7 +174,10 @@ class StayDuration implements stayDurationInterface {
 }
 
 export {
-  SaInit,
-  SaTrack,
+  saAutoTrack,
+  saTrack,
+  saSetProfile,
+  saLogin,
+  saRegisterPage,
   StayDuration
 }
